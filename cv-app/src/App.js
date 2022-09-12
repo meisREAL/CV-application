10fea9f7deb9520ef7419d98842b8e9b07ba3e6f
meisReal <simas.cesnakauskas@gmail.com>
2022-09-12 16:46:30 +0300

    personalInfo component changes state to father element App via input

--- cv-app/src/App.js
+++ cv-app/src/App.js
@@ -26,15 +26,48 @@ class App extends Component {
                 start: '',
                 end: '',
             },
+            information: [],
             educationList: [],
             workList: [],
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.onSubmitChange = this.onSubmitChange.bind(this);
     }
+
+    handleChange = (e) => {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            personalInfo: {
+                [name]: value,
+                [name]: value,
+                [name]: value,
+                [name]: value,
+            }
+        });
+    }
+
+    onSubmitChange = e => {
+        e.preventDefault();
+        this.setState({
+            information: this.state.information.concat(this.state.personalInfo),
+            personalInfo: {
+                name: '',
+                surName: '',
+                email: '',
+                phone: '',
+            }
+        })
+    }
+
     render() {
+        const { personalInfo } = this.state
         return <div id="mainWindow">
             <div id="inputFields">
-                <form>
-                    <PersonalInfo />
+                <form onSubmit={this.onSubmitChange}>
+                    <PersonalInfo personalInfo={personalInfo} handleChange={this.handleChange} />
+                    <input type='submit' />
                 </form>
             </div>
             <div id="outputFields">your formed cv</div>
--- cv-app/src/components/personalInfo.js
+++ cv-app/src/components/personalInfo.js
@@ -1,27 +1,39 @@
 import React, { Component } from "react";
 
 class PersonalInfo extends Component {
-    constructor(props) {
-        super(props);
-    }
+
     render() {
+        const { personalInfo, handleChange } = this.props;
         return (
             <div className="inputs">
                 <div className="inputFields">
                     <label>Name</label>
-                    <input type='text' name='name' />
+                    <input type='text' name='name'
+                        value={personalInfo.name}
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className="inputFields">
                     <label>Surname</label>
-                    <input type='text' name='surname' />
+                    <input type='text' name='surName'
+                        value={personalInfo.surName}
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className="inputFields">
                     <label>Email</label>
-                    <input type='email' name='email' />
+                    <input type='email' name='email'
+                        value={personalInfo.email}
+                        onChange={handleChange}
+
+                    />
                 </div>
                 <div className="inputFields">
                     <label>Phone number</label>
-                    <input type='tel' name='tel' />
+                    <input type='tel' name='phone'
+                        value={personalInfo.phone}
+                        onChange={handleChange}
+                    />
                 </div>
             </div>
         );
