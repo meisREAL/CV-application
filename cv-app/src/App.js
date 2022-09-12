5a279551d8db96d6217286fed2267948de91c7db
meisReal <simas.cesnakauskas@gmail.com>
2022-09-12 12:52:17 +0300

    state in main app to have all required fields for cv

--- cv-app/src/App.js
+++ cv-app/src/App.js
@@ -1,6 +1,34 @@
 import React, { Component } from "react";
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            personalInfo: {
+                name: '',
+                surName: '',
+                email: '',
+                phone: '',
+            },
+            education: {
+                schoolName: '',
+                subject: '',
+                degree: '',
+                start: '',
+                end: '',
+            },
+            workExperience: {
+                company: '',
+                position: '',
+                jobDescription: '',
+                start: '',
+                end: '',
+            },
+            educationList: [],
+            workList: [],
+        }
+    }
     render() {
         return <div>Hello world</div>;
     }
