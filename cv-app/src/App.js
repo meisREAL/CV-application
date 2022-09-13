f802e391f74c85618482bc96e40bfdcf624fa6b2
meisReal <simas.cesnakauskas@gmail.com>
2022-09-13 15:34:44 +0300

    App component state rewriten from objects to individual fields

--- cv-app/src/App.js
+++ cv-app/src/App.js
@@ -6,67 +6,81 @@ class App extends Component {
         super();
 
         this.state = {
-            personalInfo: {
-                name: '',
-                surName: '',
-                email: '',
-                phone: '',
-            },
-            education: {
-                schoolName: '',
-                subject: '',
-                degree: '',
-                start: '',
-                end: '',
-            },
-            workExperience: {
-                company: '',
-                position: '',
-                jobDescription: '',
-                start: '',
-                end: '',
-            },
-            information: [],
-            educationList: [],
-            workList: [],
+            // personalInfo: {
+            //     name: '',
+            //     surName: '',
+            //     email: '',
+            //     phone: '',
+            // },
+            // education: {
+            //     schoolName: '',
+            //     subject: '',
+            //     degree: '',
+            //     start: '',
+            //     end: '',
+            // },
+            // workExperience: {
+            //     company: '',
+            //     position: '',
+            //     jobDescription: '',
+            //     start: '',
+            //     end: '',
+            // },
+            // information: [],
+            // educationList: [],
+            // workList: [],
+
+            name: '',
+            surName: '',
+            email: '',
+            phone: '',
+            personalInfo: [],
+
         }
-        this.handleChange = this.handleChange.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
         this.onSubmitChange = this.onSubmitChange.bind(this);
     }
 
-    handleChange = (e) => {
+    handleInputChange = (e) => {
         const target = e.target;
         const value = target.value;
         const name = target.name;
+
         this.setState({
-            personalInfo: {
-                [name]: value,
-                [name]: value,
-                [name]: value,
-                [name]: value,
-            }
+            // personalInfo: {
+            //     // name: value,
+            //     // surName: value,
+            //     // email: value,
+            //     // phone: value,
+            // },
+            // personalInfo: {
+            //     [name]: value,
+            //     // [name]: value,
+            //     // [name]: value,
+            //     // [name]: value,
+            // }
+            [name]: value
+
         });
     }
 
     onSubmitChange = e => {
         e.preventDefault();
         this.setState({
-            information: this.state.information.concat(this.state.personalInfo),
-            personalInfo: {
-                name: '',
-                surName: '',
-                email: '',
-                phone: '',
-            }
+            personalInfo: this.state.personalInfo.concat(this.state.name, this.state.surName,
+                this.state.email, this.state.phone)
         })
+        console.log(this.personalInfo)
     }
 
     render() {
-        const { personalInfo } = this.state
+        const { name, surName, email, phone } = this.state
         return <div id="mainWindow">
             <div id="inputFields">
                 <form onSubmit={this.onSubmitChange}>
-                    <PersonalInfo personalInfo={personalInfo} handleChange={this.handleChange} />
+                    <PersonalInfo name={name} surName={surName} email={email} phone={phone}
+                        handleChange={this.handleInputChange}
+                    />
                     <input type='submit' />
                 </form>
             </div>
--- cv-app/src/components/personalInfo.js
+++ cv-app/src/components/personalInfo.js
@@ -1,38 +1,50 @@
 import React, { Component } from "react";
 
 class PersonalInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e) {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+        this.props.handleChange(e)
+        // console.log(name, value)
+    }
 
     render() {
-        const { personalInfo, handleChange } = this.props;
+        const { name, surName, email, phone } = this.props;
+
         return (
             <div className="inputs">
                 <div className="inputFields">
                     <label>Name</label>
                     <input type='text' name='name'
-                        value={personalInfo.name}
-                        onChange={handleChange}
+                        value={name}
+                        onChange={this.handleChange}
                     />
                 </div>
                 <div className="inputFields">
                     <label>Surname</label>
                     <input type='text' name='surName'
-                        value={personalInfo.surName}
-                        onChange={handleChange}
+                        value={surName}
+                        onChange={this.handleChange}
                     />
                 </div>
                 <div className="inputFields">
                     <label>Email</label>
                     <input type='email' name='email'
-                        value={personalInfo.email}
-                        onChange={handleChange}
+                        value={email}
+                        onChange={this.handleChange}
 
                     />
                 </div>
                 <div className="inputFields">
                     <label>Phone number</label>
                     <input type='tel' name='phone'
-                        value={personalInfo.phone}
-                        onChange={handleChange}
+                        value={phone}
+                        onChange={this.handleChange}
                     />
                 </div>
             </div>
